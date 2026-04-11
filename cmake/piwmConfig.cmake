6ce52cd7e7049ff23e21053d75d759c28df83e6d
Ionut Simion <ionut@simion.co>
2026-04-12 00:40:15 +0300

    Fixed some cmake issues, added the first test (WIP)

--- cmake/piwmConfig.cmake
+++ cmake/piwmConfig.cmake
@@ -1,2 +1,11 @@
 include(CMakeFindDependencyMacro)
+include(FetchContent)
+
+FetchContent_Declare(
+        typedecl
+        GIT_REPOSITORY https://github.com/ionutsimion/typelists.git
+        GIT_TAG v0.1.0
+)
+FetchContent_MakeAvailable(typedecl)
+
 include(${CMAKE_CURRENT_LIST_DIR}/piwmTargets.cmake)
--- tests/unit/core/test_window.cxx
+++ tests/unit/core/test_window.cxx
@@ -1,1 +1,16 @@
-#include <catch2/catch.hpp>
+#include <catch2/catch_test_macros.hpp>
+
+#include <piwm/window.hxx>
+
+SCENARIO("creating a window")
+{
+    GIVEN("pi::WindowManager")
+    {
+        WHEN("a window is created with default parameters")
+        {
+            auto const window = piwm::make_window();
+
+            THEN("the window should not be null.")
+        }
+    }
+}
